7d2ca4dca32c18c8b7903c4b042fa00f930a3c87
Tanner Smith <tanner@tssoftware.net>
2011-12-28 12:48:14 -0500

    Draw the title screen.

--- life.js
+++ life.js
@@ -1,10 +1,29 @@
+Mode = {
+	TITLE : 0,
+	STEP : 1,
+	STOP : 2,
+	RUN : 3
+}
+
+var currentMode = Mode.TITLE;
+
 function init() {
 	var canvas = document.getElementById("gameboy");  
 	if (canvas.getContext) {
 		var context = canvas.getContext("2d");
-		context.fillStyle = "rgb(200,0,0)";
-        context.fillRect(10, 10, 55, 50);
+		
+		update(context);
 	} else {
 		
+	}
+}
+
+function update(context) {
+	if (currentMode == Mode.TITLE) {
+		var titleImage = new Image();
+		titleImage.onload = function() {
+			context.drawImage(titleImage, 0, 0);
+		};
+		titleImage.src = 'images/title.png';
 	}
 }
