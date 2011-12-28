98ae65b05e7a4118cf1c974d82046277637fd44a
Tanner Smith <tanner@tssoftware.net>
2011-12-28 13:01:06 -0500

    Draw and functionalize menu selector.

--- js/life.js
+++ js/life.js
@@ -5,7 +5,14 @@ Mode = {
 	RUN : 3
 }
 
+Selection = {
+	FREE : 0,
+	ONE: 1,
+	TWO: 2
+}
+
 var currentMode = Mode.TITLE;
+var selection = Selection.FREE;
 
 function init() {
 	update();
@@ -22,7 +29,32 @@ function update(context) {
 				context.drawImage(titleImage, 0, 0);
 			};
 			titleImage.src = 'images/title.png';
+			
+			var newCell = new Image();
+			newCell.onload = function() {
+				context.drawImage(newCell, 71, 86 + selection * 20);
+			};
+			newCell.src = 'images/new_cell.png';
 		}
 	}
 }
 
+key('down', function() {
+	if (currentMode == Mode.TITLE) {
+		if (selection != Selection.TWO) {
+			selection++;
+		}
+	
+		update();
+	}
+});
+
+key('up', function() {
+	if (currentMode == Mode.TITLE) {
+		if (selection != Selection.FREE) {
+			selection--;
+		}
+	
+		update();
+	}
+});
