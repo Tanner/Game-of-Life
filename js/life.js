ec775773bb3b275338aaef7acc41bdf738e1824c
Tanner Smith <tanner@tssoftware.net>
2011-12-28 19:15:01 -0500

    Implement frame switching.

--- js/life.js
+++ js/life.js
@@ -62,8 +62,21 @@ function update() {
 	if (canvas.getContext) {
 		var context = canvas.getContext("2d");
 		
+		var buffer = document.createElement('canvas');
+	    buffer.width = COLS;
+	    buffer.height = ROWS;
+		
 		if (repaint) {
-			render(context);
+			context.fillStyle = 'rgb(255, 255, 255)';
+			context.fillRect(0, 0, PIXEL_SIZE, PIXEL_SIZE);
+			
+			render(buffer.getContext("2d"));
+			
+			buffer.getContext("2d").fillStyle = 'rgb(0, 0, 255)';
+			buffer.getContext("2d").fillRect(0, 0, PIXEL_SIZE, PIXEL_SIZE);
+			
+			context.drawImage(buffer, 0, 0);
+			
 			repaint = false;
 		}
 	}
